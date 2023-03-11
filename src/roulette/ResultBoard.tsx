abfc6fecf22d914a5bfe71c7befc39bd31f89cc2
Tekiter <tekiter8@gmail.com>
2023-03-12 02:32:17 +0900

    feat: change result board style

--- src/roulette/ResultBoard.tsx
+++ src/roulette/ResultBoard.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@react-three/drei";
+import { MeshTransmissionMaterial, Text } from "@react-three/drei";
 import { FC, useEffect } from "react";
 import { motion } from "framer-motion-3d";
 import { animate, useMotionValue } from "framer-motion";
@@ -15,7 +15,9 @@ const ResultBoard: FC<ResultProps> = ({ content, position }) => {
   const disabled = content === null;
 
   useEffect(() => {
-    animate(flipValue, disabled ? -Math.PI / 2 : 0);
+    animate(flipValue, disabled ? -Math.PI / 2 : 0, {
+      duration: 0.5,
+    });
   }, [disabled]);
 
   return (
@@ -26,15 +28,24 @@ const ResultBoard: FC<ResultProps> = ({ content, position }) => {
       </mesh>
       <mesh position={[0, 0.3, 0]}>
         <boxGeometry args={[5, 0.6, 0.1]} />
-        <motion.meshStandardMaterial
-          animate={{ color: disabled ? "#c3c3c3" : "#cee55d" }}
+        <MeshTransmissionMaterial
+          resolution={128}
+          samples={16}
+          roughness={0.4}
+          thickness={2}
+          transmission={1}
+          envMapIntensity={5}
+          distortion={0}
+          distortionScale={0.5}
+          temporalDistortion={0}
+          color={"#555555"}
         />
       </mesh>
       <Text
         castShadow
         receiveShadow
         font={FONT}
-        color="black"
+        color="#ffffff"
         rotation={[0, 0, 0]}
         position={[0, 0.3, 0.06]}
         fontSize={0.3}
