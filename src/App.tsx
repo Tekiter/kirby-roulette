33461d318d10fcd2dc5159a2dee89bffaec6baf5
Tekiter <tekiter8@gmail.com>
2023-05-22 21:44:33 +0900

    fix: Edit 버튼 안보임 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Suspense } from "react";
 import AmplitudeProvider from "./eventLogger/AmplitudeProvider";
 import Canvas from "./roulette/RouletteCanvas";
+import ItemList from "./roulette/ItemList";
+import MobileButtons from "./roulette/MobileButtons";
 
 function App() {
   return (
     <AmplitudeProvider apiKey={import.meta.env.VITE_AMPLITUDE_API_KEY}>
       <div className="absolute inset-0">
-        <Suspense fallback={<p>Loading...</p>}>
-          <Canvas />
-        </Suspense>
+        <div className="relative h-full">
+          <Suspense fallback={<p>Loading...</p>}>
+            <Canvas />
+          </Suspense>
+
+          <ItemList />
+          <MobileButtons />
+        </div>
       </div>
     </AmplitudeProvider>
   );
--- src/roulette/RouletteCanvas.tsx
+++ src/roulette/RouletteCanvas.tsx
@@ -2,36 +2,38 @@ import { Canvas } from "@react-three/fiber";
 import { FC } from "react";
 import Spinner from "./Spinner";
 import Camera from "./Camera";
-import ItemList from "./ItemList";
 import Light from "./Light";
 import { OrbitControls, SoftShadows } from "@react-three/drei";
-import MobileButtons from "./MobileButtons";
 
 interface RouletteProps {}
 
 const RouletteCanvas: FC<RouletteProps> = ({}) => {
   return (
     <>
-      <Canvas dpr={[1, 2]} camera={{ fov: 60, position: [100, 90, 0] }} shadows>
-        <color attach="background" args={["#e0b7ff"]} />
-        <SoftShadows />
-        <Light />
+      <div className="h-full">
+        <Canvas
+          dpr={[1, 2]}
+          camera={{ fov: 60, position: [100, 90, 0] }}
+          shadows
+        >
+          <color attach="background" args={["#e0b7ff"]} />
+          <SoftShadows />
+          <Light />
 
-        <Spinner />
-        <Camera />
+          <Spinner />
+          <Camera />
 
-        <OrbitControls
-          makeDefault
-          minAzimuthAngle={-Math.PI / 3}
-          maxAzimuthAngle={Math.PI / 4}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 2}
-          minDistance={4}
-          maxDistance={10}
-        />
-      </Canvas>
-      <ItemList />
-      <MobileButtons />
+          <OrbitControls
+            makeDefault
+            minAzimuthAngle={-Math.PI / 3}
+            maxAzimuthAngle={Math.PI / 4}
+            minPolarAngle={Math.PI / 4}
+            maxPolarAngle={Math.PI / 2}
+            minDistance={4}
+            maxDistance={10}
+          />
+        </Canvas>
+      </div>
     </>
   );
 };
